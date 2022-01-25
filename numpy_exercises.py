a641dd5b792bf1795d44a2ea4d38d9e3acc793e8
Stephen Kipkurui <stephen.kipkurui@yahoo.com>
2022-01-25 14:41:57 -0600

    Numpy Exercises

---
+++ numpy_exercises.py
@@ -0,0 +1,23 @@
+# Exercises
+
+# In your numpy-pandas-visualization-exercises repo, create a file named numpy_exercises.py for this exercise.
+
+# Use the following code for the questions below:
+
+
+# a = np.array([4, 10, 12, 23, -2, -1, 0, 0, 0, -6, 3, -7])
+# How many negative numbers are there?
+# How many positive numbers are there?
+# How many even positive numbers are there?
+# If you were to add 3 to each data point, how many positive numbers would there be?
+# If you squared each number, what would the new mean and standard deviation be?
+# A common statistical operation on a dataset is centering. This means to adjust the data such that the mean of the data is 0. This is done by subtracting the mean from each data point. Center the data set. See this link for more on centering.
+# Calculate the z-score for each data point. Recall that the z-score is given by:
+# Z
+# =
+# x
+# −
+# μ
+# σ
+# Copy the setup and exercise directions from More Numpy Practice into your numpy_exercises.py and add your solutions.
+# Awesome Bonus For much more practice with numpy, Go to https://github.com/rougier/numpy-100 and clone the repo down to your laptop. Next, go to https://github.com/new to make a new repo. Name it numpy-100, and follow the directions to "push an existing repository from the command line" so that you can push up your changes to your own account.
